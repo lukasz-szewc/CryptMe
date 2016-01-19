22d37dc4d0e237e56d4067c73bbab13dbc142a85
lukasz-szewc <lukasz-szewc@users.noreply.github.com>
2016-01-19 19:51:01 +0100

    added test that app can take byte array and encrypt later

--- src/main/java/org/luksze/glassmill/Cipher.java
+++ src/main/java/org/luksze/glassmill/Cipher.java
@@ -1,4 +1,15 @@
 package org.luksze.glassmill;
 
+import java.util.Arrays;
+
 public class Cipher {
+    private byte[] bytes;
+
+    public void encrypt(byte[] bytes, String secretPassword) {
+        this.bytes = bytes;
+    }
+
+    byte[] bytesToEncrypt() {
+        return Arrays.copyOf(bytes, bytes.length);
+    }
 }
--- src/test/java/org/luksze/glassmill/CipherTest.java
+++ src/test/java/org/luksze/glassmill/CipherTest.java
@@ -4,9 +4,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CipherTest {
+
+    public static final byte[] BYTES = "PlainText".getBytes();
+
     @Test
     public void canCreateTest() throws Exception {
         Cipher cipher = new Cipher();
         Assert.assertNotNull(cipher);
+    }
+
+    @Test
+    public void canTakeByteArrayForEncryption() throws Exception {
+        Cipher cipher = new Cipher();
+        cipher.encrypt(BYTES, "secretPassword");
+        Assert.assertArrayEquals(cipher.bytesToEncrypt(), BYTES);
     }
 }
