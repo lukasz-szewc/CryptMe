74904ddd808eeb70f845aeb030e86ee716566ab4
lukasz-szewc <lukasz-szewc@users.noreply.github.com>
2016-02-05 20:35:32 +0100

    added simple action to select file for encryption button

--- src/main/java/org/luksze/sillyfedor/gui/Gui.java
+++ src/main/java/org/luksze/sillyfedor/gui/Gui.java
@@ -1,13 +1,21 @@
 package org.luksze.sillyfedor.gui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class Gui extends Application {
+
+    private File file;
 
     public static void main(String[] args) {
         launch(args);
@@ -17,6 +25,23 @@ public class Gui extends Application {
     public void start(final Stage primaryStage) throws Exception {
         primaryStage.setTitle("SillyFedor");
         GridPane gridPane = constructGridPane();
+
+        Button encryptButton = new Button("Select file to encrypt...");
+        encryptButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                final FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Open Resource CipherFile");
+                File file = fileChooser.showOpenDialog(primaryStage);
+                if (file != null && file.exists() && file.isFile()) {
+                    Gui.this.file = file;
+//                    appStage.emitEvent(new FileHasBeenSelectedEvent());
+                }
+            }
+        });
+        gridPane.add(encryptButton, 0, 0);
+
         primaryStage.setScene(new Scene(gridPane, 770, 250));
         primaryStage.show();
     }
